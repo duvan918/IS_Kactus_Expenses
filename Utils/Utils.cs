f7b8916c88e1a840c4def29350e86d29084ba874
Yeison Franco <ydfrancor@siesa.com>
2025-04-24 17:21:19 -0500

    Div Pers Adjusted

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -12,14 +12,31 @@ public static class Utils
     public static readonly Dictionary<string, int> DepartmentMapping = new Dictionary<string, int>
     {
         { "DIGE", 196 },    //Dirección General
+
+        { "RAYS", 194 },    //Clínica
+        { "REOC", 194 },    //Clínica
+        { "RECE", 194 },    //Clínica
+        { "RENO", 194 },    //Clínica
+
+        { "FANO", 195 },    //Farma
         { "FAYS", 195 },    //Farma
-        { "RAYS", 194 }     //Clínica
+        { "FACE", 195 },    //Farma
+        { "FOCE", 195 },    //Farma
     };
 
     public static readonly Dictionary<string, string> DepartmentEquivalence = new Dictionary<string, string>
     {
         { "DIGE", "DIRECCION GENERAL" },
+
+        { "REOC", "CLINICAS" },
+        { "RENO", "CLINICAS" },
+        { "RECE", "CLINICAS" },
+        { "RAYS", "CLINICAS" },
+
         { "FAYS", "FARMA" },
-        { "RAYS", "CLINICAS" }
+        { "FANO", "FARMA" },
+        { "FACE", "FARMA" },
+        { "FOCE", "FARMA" },
+
     };
 }
